4ce599da5e1a2e9f2aa8390c8eb175bbdcece8b6
krait8064 <krait8063@163.com>
2025-08-26 17:06:50 +0800

    1.add work log 0826 2.target skills doc finished

--- interview-cases/src/main/java/org/example/Main.java
+++ interview-cases/src/main/java/org/example/Main.java
@@ -1,17 +1,57 @@
 package org.example;
 
+import java.io.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static final String SEP = ";";
+    public static void main(String[] args) {
+        extractOfferRequirements(args);
+    }
+
+    /**
+     * extract from excel recorded data
+     * @param args args[0] readfile path in ../../resources/skills.csv,write file path in ../../resources/keys.txt
+     */
+    private static void extractOfferRequirements(String[] args) {
+        String readPath = args[0];
+        String writePath = args[1];
+        System.out.println (readPath);
+        System.out.println (writePath);
+        File writeFile = new File(writePath);
+        if(!writeFile.exists()){
+            try {
+                if (!writeFile.createNewFile()){
+                    System.out.println ("create write file failed");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }else {
+            try {
+                new FileWriter(writeFile, false).close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(readPath));BufferedWriter bw = new BufferedWriter(new FileWriter(writePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                line = line.toLowerCase();
+                if (!line.isEmpty()) {
+                    String[] keys = line.split(SEP);
+                    for (String key : keys) {
+                        String formatKey = key.trim().toLowerCase();
+                        if (!formatKey.isEmpty()) {                        bw.write(formatKey);
+                            bw.newLine();}
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
