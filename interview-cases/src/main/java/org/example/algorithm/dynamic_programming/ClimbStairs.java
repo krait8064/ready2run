2a2249dba9745755637c8c06c104253962a77ce5
krait8064 <krait8063@163.com>
2025-09-03 21:42:58 +0800

    add log 0903

---
+++ interview-cases/src/main/java/org/example/algorithm/dynamic_programming/ClimbStairs.java
@@ -0,0 +1,19 @@
+package org.example.algorithm.dynamic_programming;
+
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        if (n <= 3) return n;
+
+        int prev1 = 3;
+        int prev2 = 2;
+        int cur = 0;
+
+        for (int i = 3; i < n; i++) {
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return cur;
+    }
+}
